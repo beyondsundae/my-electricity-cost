cffec87b94bf9bc2beb544a036670f36a161cb93
DESKTOP-TT11N69\p0000 <p00000rice@gmail.com>
2022-03-13 03:47:26 +0700

    change to classname because cant build

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -4,7 +4,7 @@ import Header from './Header'
 
 const Layout = (props) => {
     return (
-        <div class='bg-main'>
+        <div className='bg-main'>
               {/* <Header/> */}
               <div>
                 {props.children}
